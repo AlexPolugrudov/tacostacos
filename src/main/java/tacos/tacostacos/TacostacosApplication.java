2ad1e03d0f27260e949968772b3c5647ecbba313
AlexPolugrudov <sasha.polugrudov@gmail.com>
2022-05-30 17:16:47 +0300

    FSP

---
+++ src/main/java/tacos/tacostacos/TacostacosApplication.java
@@ -0,0 +1,13 @@
+package tacos.tacostacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacostacosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacostacosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tacos/tacostacos/TacostacosApplicationTests.java
@@ -0,0 +1,13 @@
+package tacos.tacostacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacostacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
